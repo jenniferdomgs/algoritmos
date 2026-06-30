7be12726b994e1797669d958d82e89f480633c1a
jennifer <99359081+jenniferdomgs@users.noreply.github.com>
2026-06-30 10:31:24 -0300

    q1 backtraking

--- backtraking/q1.cpp
+++ backtraking/q1.cpp
@@ -1,8 +1,8 @@
 # include <iostream>
 
-int caminhoValido(int labirinto[8][8], int linha, int coluna, int l, int c) {]
-    // se linha ou coluna for menor que 0 ou maior que o tamanho do labirinto ou se a posção for 1 -> retorna 0
-    if (l<0 || c<0 || l>=linha || c>=coluna || labirinto[l][c]==1) {
+int caminhoValido(int labirinto[6][6], bool visitado[6][6], int linha, int coluna, int l, int c) {
+    // se é fora dos limites, se é parede ou se já visitou
+    if (l<0 || c<0 || l>=linha || c>=coluna || labirinto[l][c]==1 || visitado[1][c]) {
         return 0;
     }
     
@@ -10,11 +10,21 @@ int caminhoValido(int labirinto[8][8], int linha, int coluna, int l, int c) {]
         return 1;
     }
 
-    if (caminhoValido(labirinto, linha, coluna, l+1, c)) {
+    visitado[l][c] = true; // já visitou
+
+    if (caminhoValido(labirinto, visitado, linha, coluna, l + 1, c)) { // baixo
         return 1;
     }
 
-    if (caminhoValido(labirinto, linha, coluna, l, c+1)) {
+    if (caminhoValido(labirinto, visitado, linha, coluna, l - 1, c)) { // cima
+        return 1;
+    }
+
+    if (caminhoValido(labirinto, visitado, linha, coluna, l, c + 1)) { // direita
+        return 1;
+    }
+
+    if (caminhoValido(labirinto, visitado, linha, coluna, l, c - 1)) { // esquerda
         return 1;
     }
 
@@ -22,14 +32,17 @@ int caminhoValido(int labirinto[8][8], int linha, int coluna, int l, int c) {]
 }
 
 int main() {
-    int linha, coluna, labirinto[8][8];
+    int linha, coluna, labirinto[6][6];
+    bool visitado[6][6] = {false}; // tudo sem ser visitado
+
     std::cin >> linha >> coluna;
     for (int i = 0; i < linha; i++) {
         for (int j = 0; j < coluna; j++) {
             std::cin >> labirinto[i][j];
         }
     }
-    if (caminhoValido(labirinto, linha, coluna, 0,0)) {
+
+    if (caminhoValido(labirinto, visitado, linha, coluna, 0, 0)) {
         std::cout << "Existe um caminho válido!" << std::endl;
     } else {
         std::cout << "Não existe caminho válido!" << std::endl;
